84492148cf22ea9f43038d40fe958b2abe63a62e
Garrett Bond <garrett.bond@intimetec.com>
2025-07-15 23:44:09 -0600

    Button Component Dynamically created for future pages with basic mobile and web designs made.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-
+import Button from '../components/Button';
 
 const LandingPage: React.FC = () => {
   return (
@@ -12,6 +12,7 @@ const LandingPage: React.FC = () => {
       <div className="mt-6">
         <Link to="/board/1" className="text-blue-500 underline hover:text-blue-700">Go to Board 1</Link>
       </div>
+      <Button className="bg-blue-500 text-white">Create</Button>
     </div>
   );
 };

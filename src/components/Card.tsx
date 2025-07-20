51c06ad45cf9c164e4ece7fedbab9f234e6f82f4
Garrett Bond <garrett.bond@intimetec.com>
2025-07-19 21:40:05 -0600

    Made responsive changes to Navbar, Card, and LandingPage

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,12 +5,12 @@ type CardProps = {
 
 function Card({ children, className, ...props }: CardProps) {
   return (
-    <div className={`rounded-xl size-70 flex flex-col ${className}`} {...props}>
-        <div id="headerConter" className="rounded-t-xl border-ash border-t border-x flex flex-row justify-start items-center bg-snow h-20">
-            <p className="mx-4 rounded-full size-10 bg-lavender text-nurple font-bold flex items-center justify-center">A</p>
-            <h3>Board A</h3>
+    <div className={`rounded-xl size-65 flex flex-shrink-0 flex-col shadow-md ${className}`} {...props}>
+        <div id="headerConter" className="rounded-t-xl border-ash border-t border-x flex flex-row justify-start items-center bg-snow h-17 pl-3">
+            <p className="hidden lg:flex lg:mr-3 rounded-full size-10 bg-lavender text-nurple font-bold items-center justify-center">A</p>
+            <h3 className="font-semibold">Board A</h3>
         </div>
-        <div className="bg-mist h-50 rounded-b-xl"></div>
+        <div className="bg-mist h-48 rounded-b-xl"></div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,30 @@
-import logo from '../assets/choriLogo.png'
-import userProfile from '../assets/userProfile.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faFlipboard } from '@fortawesome/free-brands-svg-icons'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons/faCircleUser'
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 lg:h-16 left-0 w-full grid grid-cols-6 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-3 p-2 border-b border-gray-300">
+    <div className="fixed top-0 h-16 left-0 w-full grid grid-cols-6 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-3 p-2 border-b-3 border-lavender">
         <div className='flex items-center justify-start'>
-            <img className="w-10 h-10" src={logo} alt="logo" />
+            {/* <img className="w-10 h-10" src={logo} alt="logo" /> */}
+            <FontAwesomeIcon icon={faFlipboard} className="text-4xl" />
             <h1 className="hidden lg:block text-4xl font-bold pl-2">Chori</h1>
         </div>
         <form className='col-span-4 lg:col-span-3 xl:col-span-2 2xl:col-span-1 flex items-center'>
             <div className="relative w-full">
-                <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                    <FontAwesomeIcon icon={faSearch} className="w-5 h-5" />
+                <span className="hidden md:block md:absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+                    <FontAwesomeIcon icon={faSearch} className="text-lg" />
                 </span>
                 <input
-                    className="rounded-full border-2 border-gray-300 p-2 pl-10 w-full"
+                    className="rounded-full border-2 text-black border-gray-300 h-10 pl-3 md:pl-10 w-full"
                     type="text"
                     placeholder="Search"
                 />
             </div>
         </form>
         <div className='flex items-center justify-end'>
-            <img className='w-10 h-10' src={userProfile} alt="user profile" />
+            <FontAwesomeIcon className='text-4xl text-nurple' icon={faCircleUser}/>
         </div>
     </div>
   )

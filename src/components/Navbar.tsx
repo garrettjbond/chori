324a9f14bd040a9cae423ca39cf7ef71325ddb05
Garrett Bond <garrett.bond@intimetec.com>
2025-07-23 22:46:59 -0600

    Finished the mobile and desktop verson of the TaskBar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faFlipboard } from '@fortawesome/free-brands-svg-icons'
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons/faCircleUser'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 h-16 left-0 w-full grid grid-cols-6 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-3 p-2 border-b-5 border-lavender">
+    <div className="fixed top-0 h-16 left-0 w-full grid grid-cols-6 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-3 p-2 lg:px- border-b-5 border-lavender">
         <div className='flex items-center justify-start'>
-            {/* <img className="w-10 h-10" src={logo} alt="logo" /> */}
-            <FontAwesomeIcon icon={faFlipboard} className="text-4xl" />
-            <h1 className="hidden lg:block text-4xl font-bold pl-2">Chori</h1>
+            <Link to="/" className='cursor-pointer hover:text-nurple duration-300'>
+                <span className="flex items-center">
+                    <FontAwesomeIcon icon={faFlipboard} className="text-4xl" />
+                    <h1 className="hidden lg:block text-4xl font-bold pl-2">Chori</h1>
+                </span>
+            </Link>
         </div>
         <form className='col-span-4 lg:col-span-3 xl:col-span-2 2xl:col-span-1 flex items-center'>
             <div className="relative w-full">
---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,27 @@
+import { faCaretRight, faEllipsis, faFilter, faGear, faPeopleGroup, faStar, faUserPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Button from "./Button"
+
+const Taskbar = () => {
+    return (
+        <div className="fixed top-16 h-13 left-0 w-full pl-4 pr-2 lg:pl-8 lg:pr-6 border-b-5 border-lavender flex justify-between items-center">
+            <div className="flex items-center gap-2">
+                <p className="font-semibold">Board A</p>
+                <div className="hidden sm:block sm:flex gap-2">
+                    <p className="cursor-pointer hover:text-nurple duration-300"><span className='pl-3 pr-1'><FontAwesomeIcon className="text-darkAsh" icon={faCaretRight}></FontAwesomeIcon></span></p>
+                    <p className="cursor-pointer hover:text-nurple duration-300"><span className='pl-3 pr-1'><FontAwesomeIcon icon={faGear}></FontAwesomeIcon></span>Settings</p>
+                    <p className="cursor-pointer hover:text-nurple duration-300"><span className='pl-3 pr-1'><FontAwesomeIcon icon={faPeopleGroup}></FontAwesomeIcon></span>Members</p>
+                    <p className="cursor-pointer hover:text-nurple duration-300"><span className='pl-3 pr-1'><FontAwesomeIcon icon={faStar} /></span>Favorite</p>
+                </div>
+            </div>
+            <div className="flex items-center">
+                <span  className="md:hidden text-2xl pr-2"><FontAwesomeIcon icon={faEllipsis} />
+                </span>
+                <span  className="hidden md:block text-2xl pr-2 cursor-pointer hover:text-nurple duration-300"><FontAwesomeIcon icon={faFilter} /></span>
+                <Button size="small" className="hidden md:block bg-nurple text-white"><FontAwesomeIcon icon={faUserPlus} className="pr-1 text-sm"></FontAwesomeIcon>Share</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Taskbar

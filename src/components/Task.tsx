d704a1156818bda8c705f5c7c42c69dd0ca002d8
Garrett Bond <garrett.bond@intimetec.com>
2025-08-29 10:58:22 -0600

    Added small user faIcon to Tasks component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,4 @@
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type TaskProps = {
@@ -9,7 +9,10 @@ type TaskProps = {
 function Task({ children, className }: TaskProps) {
     return (
         <div className={`task group flex cursor-pointer hover:text-nurple duration-300 justify-between bg-white border border-gray-300 px-3 rounded-md h-11 flex items-center ${className || ''}`}>
-            {children || <p>Task 1</p>}
+            <div className="flex items-center">
+                <FontAwesomeIcon className='text-2xl text-darkAsh pr-2' icon={faCircleUser} />
+                {children || <p>Task 1</p>}
+            </div>
             <FontAwesomeIcon icon={faPenToSquare} className="opacity-0 group-hover:opacity-100 hover:animate-pulse text-lg transition-opacity duration-200" />
         </div>
     );
